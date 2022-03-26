0402c68137b3380ef80f8274a2167fdf95fadfc8
IronJin <85132068+IronJin@users.noreply.github.com>
2022-03-26 16:08:34 +0900

    Four commit

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,21 +2,25 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
+//컨트롤 + 쉬프트 + T 를 눌러서 테스트를 생성해줄 수 있음
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //회원가입 함수
     //비지니스 로직(rule) : 중복회원이 있으면 안되므로 같은이름을 가진 중복회원은 안됨
     public Long join(Member member){
 
         //같은 이름이 있는 중복회원을 받지 않음
+        //Refactor를 사용하여 함수화 해준것임
         validateduplicatemember(member);
         //중복회원을 검사하고 없다면 저장해준다.
         memberRepository.save(member);
